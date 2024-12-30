fc378a08862b721df099d9dc34961060f06523eb
ali saghir <alisaghir543@gmail.com>
2024-12-30 13:24:12 +0400

    edited the products page plus finishing the about us page

--- rayn-look/app/about/page.jsx
+++ rayn-look/app/about/page.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { fadeIn, slideIn, zoomIn } from "@/variants";
+import Link from "next/link";
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
-  )
-}
+    <section className="pt-10 overflow-hidden bg-black md:pt-0 sm:pt-16 2xl:pt-16">
+      <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+        <div className="grid items-center grid-cols-1 md:grid-cols-2">
+          {/* Left Section */}
+          <div className="text-center md:text-left">
+            <motion.div
+              variants={slideIn("down", 0.8)}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: false, amount: 0.4 }}
+            >
+              <h2 className="text-3xl font-bold leading-tight text-customGold sm:text-4xl lg:text-5xl">
+                Meet The Owner
+              </h2>
+              <h2 className="text-3xl font-bold leading-tight text-customGold sm:text-4xl lg:text-5xl mt-4">
+                Rayan Saghir
+              </h2>
+            </motion.div>
+            <div></div>
+            <motion.p
+              variants={fadeIn("left", 0.8)}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: false, amount: 0.4 }}
+              className="max-w-lg mt-3 text-xl leading-relaxed text-white md:mt-8"
+            >
+              Founded in 2014 by Rayan Saghir, our story began in the heart of
+              our home. With passion, dedication, and an unwavering belief in
+              quality, we turned a small dream into a thriving business. Today,
+              we’re proud to be recognized worldwide for offering a variety of
+              the most natural and stunning contact lens colors.
+            </motion.p>
 
-export default AboutUs
+            <motion.p
+              variants={fadeIn("left", 1)}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: false, amount: 0.4 }}
+              className="max-w-lg mt-3 text-xl leading-relaxed text-white md:mt-8"
+            >
+              Our products are designed to blend seamlessly with your natural
+              beauty, enhancing your look while providing unmatched comfort.
+              Whether you're seeking subtle elegance or a bold transformation,
+              we have the perfect choice for you.
+            </motion.p>
+
+            <motion.p
+              variants={fadeIn("left", 1.2)}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: false, amount: 0.4 }}
+              className="mt-4 text-xl text-white md:mt-8"
+            >
+              <span className="relative inline-block">
+                <span className="absolute inline-block w-full bottom-0.5 h-2 bg-customGold"></span>
+                <span className="relative"> Got a question? </span>
+              </span>
+              <br className="block sm:hidden" />
+              Feel free to{" "}
+              <Link
+                href="/contact"
+                title=""
+                className="transition-all duration-200 text-customGold hover:text-white hover:underline"
+              >
+                Contact Us
+              </Link>{" "}
+              or explore more about us!
+            </motion.p>
+          </div>
+
+          {/* Right Section (Image) */}
+          <motion.div
+            variants={zoomIn("up", 1)}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false, amount: 0.4 }}
+            className="relative"
+          >
+            <img
+              className="relative w-full xl:max-w-lg xl:mx-auto 2xl:origin-bottom 2xl:scale-110"
+              src="https://cdn.rareblocks.xyz/collection/celebration/images/team/1/business-woman.png"
+              alt="Founder Rayan Saghir"
+            />
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutUs;
--- rayn-look/app/products/page.jsx
+++ rayn-look/app/products/page.jsx
@@ -1,12 +1,67 @@
+"use client";
 import React from "react";
 import { MdAddShoppingCart } from "react-icons/md";
 import Image from "next/image";
-import hero from '../../public/Hero.jpg'
+import hero from "../../public/Hero.jpg";
+import { motion } from "framer-motion";
+import { fadeIn, slideIn, zoomIn } from "@/variants";
+import Link from "next/link";
+
+const products = [
+  {
+    id: 1,
+    name: "Emerald",
+    description: "With Power",
+    price: "$30",
+    image: hero,
+  },
+  {
+    id: 2,
+    name: "Gray Lava",
+    description: "With Power",
+    price: "$30",
+    image: hero,
+  },
+  {
+    id: 3,
+    name: "Barbie Blue",
+    description: "With Power",
+    price: "$30",
+    image: hero,
+  },
+  {
+    id: 4,
+    name: "Green Hazel",
+    description: "With Power",
+    price: "$30",
+    image: hero,
+  },
+  {
+    id: 5,
+    name: "Silver",
+    description: "With Power",
+    price: "$30",
+    image: hero,
+  },
+  {
+    id: 6,
+    name: "Aurora Green",
+    description: "With Power",
+    price: "$30",
+    image: hero,
+  },
+];
 
 const Products = () => {
   return (
     <>
-      <div className="text-center pt-20">
+      <motion.div
+        variants={slideIn("down", 1)}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, amount: 0.4 }}
+        className="text-center pt-20"
+      >
         <h1 className="font-bold text-4xl mb-4 text-white">
           Enhance Your Vision with Confidence
         </h1>
@@ -14,166 +69,46 @@ const Products = () => {
           Explore our premium contact lenses for clarity, comfort, and
           style—perfect for every need.
         </p>
-      </div>
+      </motion.div>
 
       <section
-        id="Projects"
-        className="mb-20 w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 "
+        id="Products"
+        className="mb-20 w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10"
       >
-        <div className="w-72 bg-dark shadow-md rounded-lg duration-500 hover:scale-105 hover:shadow-xl">
-          <a href="#">
-            <Image
-              src={hero}
-              alt="Product"
-              className="h-80 w-72 object-cover rounded-t-xl"
-            />
-            <div className="px-4 py-3 w-72">
-              <span className="text-gray-400 mr-3 uppercase text-xs">
-                With Power
-              </span>
-              <p className="text-lg font-bold text-white truncate block capitalize">
-                Emerald
-              </p>
-              <div className="flex items-center">
-                <p className="text-lg font-semibold text-white cursor-auto my-3">
-                  $30
+        {products.map((product) => (
+          <motion.div
+            variants={zoomIn("up", 1)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.4 }}
+            key={product.id}
+            className="w-80 bg-dark shadow-md rounded-lg duration-500 hover:scale-105 hover:shadow-xl"
+          >
+            <Link href="#">
+              <Image
+                src={product.image}
+                alt="Product"
+                className="h-80 w-80 object-cover rounded-t-xl"
+              />
+              <div className="px-4 py-3 w-80">
+                <span className="text-gray-400 mr-3 uppercase text-xs">
+                  {product.description}
+                </span>
+                <p className="text-lg font-bold text-white truncate block capitalize">
+                  {product.name}
                 </p>
-                <div className="ml-auto text-2xl transition-all duration-300 ease-in-out text-white border border-white p-2 rounded-lg hover:bg-customGold">
-                  <MdAddShoppingCart />
+                <div className="flex items-center">
+                  <p className="text-lg font-semibold text-white cursor-auto my-3">
+                    {product.price}
+                  </p>
+                  <div className="ml-auto text-2xl transition-all duration-300 ease-in-out text-white border border-customGold p-2 rounded-lg hover:bg-customGold">
+                    <MdAddShoppingCart />
+                  </div>
                 </div>
               </div>
-            </div>
-          </a>
-        </div>
-
-        <div className="w-72 bg-dark shadow-md rounded-lg duration-500 hover:scale-105 hover:shadow-xl">
-          <a href="#">
-            <Image
-              src={hero}
-              alt="Product"
-              className="h-80 w-72 object-cover rounded-t-xl"
-            />
-            <div className="px-4 py-3 w-72">
-              <span className="text-gray-400 mr-3 uppercase text-xs">
-                With Power
-              </span>
-              <p className="text-lg font-bold text-white truncate block capitalize">
-                Gray lava
-              </p>
-              <div className="flex items-center">
-                <p className="text-lg font-semibold text-white cursor-auto my-3">
-                  $30
-                </p>
-                <div className="ml-auto text-2xl transition-all duration-300 ease-in-out text-white border border-white p-2 rounded-lg hover:bg-customGold">
-                  <MdAddShoppingCart />
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-        <div className="w-72 bg-dark shadow-md rounded-lg duration-500 hover:scale-105 hover:shadow-xl">
-          <a href="#">
-            <Image
-              src={hero}
-              alt="Product"
-              className="h-80 w-72 object-cover rounded-t-xl"
-            />
-            <div className="px-4 py-3 w-72">
-              <span className="text-gray-400 mr-3 uppercase text-xs">
-                With Power
-              </span>
-              <p className="text-lg font-bold text-white truncate block capitalize">
-                Barbie Blue
-              </p>
-              <div className="flex items-center">
-                <p className="text-lg font-semibold text-white cursor-auto my-3">
-                  $30
-                </p>
-                <div className="ml-auto text-2xl transition-all duration-300 ease-in-out text-white border border-white p-2 rounded-lg hover:bg-customGold">
-                  <MdAddShoppingCart />
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-
-        <div className="w-72 bg-dark shadow-md rounded-lg duration-500 hover:scale-105 hover:shadow-xl">
-          <a href="#">
-            <Image
-            src={hero}
-              alt="Product"
-              className="h-80 w-72 object-cover rounded-t-xl"
-            />
-            <div className="px-4 py-3 w-72">
-              <span className="text-gray-400 mr-3 uppercase text-xs">
-                With Power
-              </span>
-              <p className="text-lg font-bold text-white truncate block capitalize">
-                Green hazel
-              </p>
-              <div className="flex items-center">
-                <p className="text-lg font-semibold text-white cursor-auto my-3">
-                  $30
-                </p>
-                <div className="ml-auto text-2xl transition-all duration-300 ease-in-out text-white border border-white p-2 rounded-lg hover:bg-customGold">
-                  <MdAddShoppingCart />
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-
-        <div className="w-72 bg-dark shadow-md rounded-lg duration-500 hover:scale-105 hover:shadow-xl">
-          <a href="#">
-            <Image
-              src={hero}
-              alt="Product"
-              className="h-80 w-72 object-cover rounded-t-xl"
-            />
-            <div className="px-4 py-3 w-72">
-              <span className="text-gray-400 mr-3 uppercase text-xs">
-                With Power
-              </span>
-              <p className="text-lg font-bold text-white truncate block capitalize">
-                Silver
-              </p>
-              <div className="flex items-center">
-                <p className="text-lg font-semibold text-white cursor-auto my-3">
-                  $30
-                </p>
-                <div className="ml-auto text-2xl transition-all duration-300 ease-in-out text-white border border-white p-2 rounded-lg hover:bg-customGold">
-                  <MdAddShoppingCart />
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-
-        <div className="w-72 bg-dark shadow-md rounded-lg duration-500 hover:scale-105 hover:shadow-xl">
-          <a href="#">
-            <Image
-              src={hero}
-              alt="Product"
-              className="h-80 w-72 object-cover rounded-t-xl"
-            />
-            <div className="px-4 py-3 w-72">
-              <span className="text-gray-400 mr-3 uppercase text-xs">
-                With Power
-              </span>
-              <p className="text-lg font-bold text-white truncate block capitalize">
-                Aurora Green
-              </p>
-              <div className="flex items-center">
-                <p className="text-lg font-semibold text-white cursor-auto my-3">
-                  $30
-                </p>
-                <div className="ml-auto text-2xl transition-all duration-300 ease-in-out text-white border border-white p-2 rounded-lg hover:bg-customGold">
-                  <MdAddShoppingCart />
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
+            </Link>
+          </motion.div>
+        ))}
       </section>
     </>
   );
